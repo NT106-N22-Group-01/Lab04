8a3cb83252dbc2b116e8b1a9f8e32167f88f54ca
tuilakhanh <bacdau852@gmail.com>
2023-05-22 19:20:32 +0700

    Inital

---
+++ Lab04/MainForm.cs
@@ -0,0 +1,49 @@
+namespace Lab04
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+			this.FormClosed += (sender, e) => Application.Exit();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void buttonEx01_Click(object sender, EventArgs e)
+		{
+			this.Hide();
+			var Ex01 = new task01();
+			Ex01.Show();
+		}
+
+		private void buttonEx2_Click(object sender, EventArgs e)
+		{
+			this.Hide();
+			var Ex02 = new Lab03.Ex02.Ex02();
+			Ex02.Show();
+		}
+
+		private void buttonEx3_Click(object sender, EventArgs e)
+		{
+			this.Hide();
+			var Ex03 = new Lab03.Ex03.Ex03();
+			Ex03.Show();
+		}
+
+		private void buttonEx4_Click(object sender, EventArgs e)
+		{
+			this.Hide();
+			var Ex04 = new Lab03.Ex04.Landing_04();
+			Ex04.Show();
+		}
+
+		private void buttonExit_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+	}
+}
