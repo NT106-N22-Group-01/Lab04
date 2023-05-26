fa3ed6d0cba4ea216e58e38d7fb4f639f39a6a07
NhatGit <nhatnm3d@gmail.com>
2023-05-26 19:07:27 +0700

    Chưa xong

---
+++ Ex06/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Ex06
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wbNews = new System.Windows.Forms.WebBrowser();
+            this.tbURL = new System.Windows.Forms.TextBox();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // wbNews
+            // 
+            this.wbNews.Location = new System.Drawing.Point(12, 40);
+            this.wbNews.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wbNews.Name = "wbNews";
+            this.wbNews.Size = new System.Drawing.Size(776, 398);
+            this.wbNews.TabIndex = 0;
+            // 
+            // tbURL
+            // 
+            this.tbURL.Location = new System.Drawing.Point(12, 15);
+            this.tbURL.Name = "tbURL";
+            this.tbURL.Size = new System.Drawing.Size(697, 20);
+            this.tbURL.TabIndex = 1;
+            // 
+            // btnGet
+            // 
+            this.btnGet.Location = new System.Drawing.Point(715, 12);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(72, 22);
+            this.btnGet.TabIndex = 2;
+            this.btnGet.Text = "Get";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGet);
+            this.Controls.Add(this.tbURL);
+            this.Controls.Add(this.wbNews);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser wbNews;
+        private System.Windows.Forms.TextBox tbURL;
+        private System.Windows.Forms.Button btnGet;
+    }
+}
+
---
+++ Ex06/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex06
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(tbURL.Text.Trim()))
+                return;
+
+            try
+            {
+                wbNews.Navigate(tbURL.Text.Trim());
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
